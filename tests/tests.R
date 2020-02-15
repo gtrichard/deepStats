1641ade1e387d5b7deeb6e1d5f4c313f1ed7e926
Gautier RICHARD <26085589+gtrichard@users.noreply.github.com>
2020-02-15 02:29:05 +0100

    Update tests.R

--- tests/tests.R
+++ tests/tests.R
@@ -1,7 +1,6 @@
 library("tools")
 setwd(dir = "tests")
 
-system("../bin/dsComputeBEDDensity --input input/genome_genes.bed --chromSize input/genome_genes_chrom.sizes --windowSize 1000 -o output/tmp_dsComputeBEDDensity")
 cat("dsComputeBEDDensity run finished\n")
 md5sum("output/tmp_dsComputeBEDDensity.bedGraph")
 md5sum("output/res_dsComputeBEDDensity.bedGraph")
@@ -9,7 +8,6 @@ if ( md5sum("output/tmp_dsComputeBEDDensity.bedGraph") != md5sum("output/res_dsC
   stop("dsComputeBEDDensity output is not as expected.")
 }
 
-system("../bin/dsComputeGCCoverage --input input/genome.fa --windowSize 1000 -o output/tmp_dsComputeGCCoverage")
 cat("dsComputeGCCoverage run finished\n")
 md5sum("output/tmp_dsComputeGCCoverage.bedGraph")
 md5sum("output/res_dsComputeGCCoverage.bedGraph")
@@ -17,10 +15,9 @@ if ( md5sum("output/tmp_dsComputeGCCoverage.bedGraph") != md5sum("output/res_dsC
   stop("dsComputeGCCoverage output is not as expected.")
 }
 
-system("../bin/dsCompareCurves -i input/deeptools_matrix.txt -o output/tmp_dsCompareCurves -b 50")
 cat("dsCompareCurves run finished\n")
 md5sum("output/tmp_dsCompareCurves.dscc")
-md5sum("output/tmp_dsCompareCurves.dscc")
+md5sum("output/res_dsCompareCurves.dscc")
 if ( md5sum("output/tmp_dsCompareCurves.dscc") != md5sum("output/res_dsCompareCurves.dscc") ){
   stop("dsCompareCurves output is not as expected.")
 }
