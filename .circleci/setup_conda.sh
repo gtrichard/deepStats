78a94d3607af0a4c50483c7b0255453f81d8d1ec
Gautier RICHARD <26085589+gtrichard@users.noreply.github.com>
2020-02-15 01:18:12 +0100

    Update setup_conda.sh

--- .circleci/setup_conda.sh
+++ .circleci/setup_conda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
